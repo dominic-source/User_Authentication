13d726a37f4e5793cbd54a10d9a08d39c80284a7
dominic-source <chinonsodomnic@gmail.com>
2024-07-06 23:04:16 +0100

    create user model

--- models/user.py
+++ models/user.py
@@ -2,12 +2,16 @@ from app import db
 
 class User(db.Model):
     """
-    This is the Category class. It represents a category in the database.
+    This is the user class. It represents a user in the database.
 
     Attributes:
-        id (int): The primary key of the category.
-        name (str): The name of the category. It is unique and cannot be null.
+        userId (str): The primary key of the user.
+        email (str): The email of user. It is unique and cannot be null.
     """
-    __tablename__ = 'category'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(35), unique=True, nullable=False)
+    __tablename__ = 'user'
+    userId = db.Column(db.String, primary_key=True, unique=True)
+    firstName = db.Column(db.String, nullable=False)
+    lastName = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    phone = db.Column(db.String)
