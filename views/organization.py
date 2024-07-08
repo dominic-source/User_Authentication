2c91b16fc38fe26c5f3f506c5c39f7406eab8cec
dominic-source <chinonsodomnic@gmail.com>
2024-07-08 22:29:31 +0100

    create organization views

--- views/organization.py
+++ views/organization.py
@@ -10,36 +10,54 @@ from app import db
 @protected_route
 def add_existing_user_to_organization(orgId=None):
     """This is the add existing user to organization function"""
-    user_id = session.get('user_id')
-    user = User.query.filter_by(userId=user_id).first()
-    if user is None:
+    try:
+        orgOwner = session.get('user_id')
+        orgUser = User.query.filter_by(userId=orgOwner).first()
+        if orgUser is None:
+            return jsonify({
+                "status": "Bad request",
+                "message": "User not found",
+                "statusCode": 404
+            }), 404
+        request_data = request.get_json()
+        userId = request_data.get('userId')
+        user = User.query.filter_by(userId=userId).first()
+        print("i got here 111111111111111111113333333333333")
+
+        if user is None:
+            return jsonify({
+                "status": "Bad request",
+                "message": "The User was not found",
+                "statusCode": 404
+            }), 404
+        org_check = Organization.query.filter_by(orgId=orgId).first()
+        if org_check is None:
+            return jsonify({
+                "status": "Bad request",
+                "message": "Organization not found",
+                "statusCode": 404
+            }), 404
+        organization = [org for org in orgUser.organizations if org.orgId == orgId]
+        if not organization:
+            return jsonify({
+                "status": "Bad request",
+                "message": "You are not in this organization",
+                "statusCode": 400
+            }), 400
+        
+        user.organizations.append(org_check)
+        db.session.commit()
+        return jsonify({
+            "status": "success",
+            "message": "User added to organization successfully"
+        }), 200
+    except Exception as e:
+        print(e)
         return jsonify({
             "status": "Bad request",
-            "message": "User not found",
-            "statusCode": 404
-        }), 404
-    organization = user.organizations.filter_by(orgId=orgId).first()
-    if organization is None:
-        return jsonify({
-            "status": "Bad request",
-            "message": "Organization not found",
-            "statusCode": 404
-        }), 404
-    request_data = request.get_json()
-    userId = request_data.get('userId')
-    user = User.query.filter_by(userId=userId).first()
-    if user is None:
-        return jsonify({
-            "status": "Bad request",
-            "message": "User not found",
-            "statusCode": 404
-        }), 404
-    user.organizations.append(organization)
-    db.session.commit()
-    return jsonify({
-        "status": "success",
-        "message": "User added to organization successfully"
-    }), 200
+            "message": "Server error",
+            "statusCode": 400
+        }), 400
 
 
 @app_views.route("/api/organisations", methods=["POST"])
@@ -59,13 +77,14 @@ def create_organization():
     description = request_data.get('description')
 
     try:
-        organization = user.organizations.filter_by(name=name).first()
-        if organization is not None:
+        organization = [orgName for orgName in user.organizations if orgName.name == name]
+        if organization:
             return jsonify({
                 "status": "Bad Request",
                 "message": "Client error",
                 "statusCode": 400
         }), 400
+
         organization = Organization(orgId=str(uuid.uuid4()), name=name, description=description)
         user.organizations.append(organization)
         db.session.add(organization)
