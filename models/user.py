0bf0b325f0a39a4eda881f1aa46d1b6e483437e8
dominic-source <chinonsodomnic@gmail.com>
2024-07-08 22:29:04 +0100

    create user or organization models

--- models/user.py
+++ models/user.py
@@ -1,5 +1,11 @@
 from app import db
 
+# Association table for the many-to-many relationship
+user_organization = db.Table('user_organization',
+    db.Column('user_id', db.String, db.ForeignKey('user.userId'), primary_key=True),
+    db.Column('organization_id', db.String, db.ForeignKey('organization.orgId'), primary_key=True)
+)
+
 class User(db.Model):
     """
     This is the user class. It represents a user in the database.
@@ -15,7 +21,8 @@ class User(db.Model):
     email = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
     phone = db.Column(db.String)
-    
+    organizations = db.relationship('Organization', secondary=user_organization, backref=db.backref('users', lazy='dynamic'))
+
 
 class Organization(db.Model):
     """This class is the organization class where users can belong"""
@@ -23,4 +30,3 @@ class Organization(db.Model):
     orgId = db.Column(db.String, primary_key=True, unique=True)
     name = db.Column(db.String, nullable=False)
     description = db.Column(db.String, nullable=True)
-    users = db.relationship('User', backref='organizations', cascade='all, delete-orphan')
