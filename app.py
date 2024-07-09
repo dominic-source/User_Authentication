506d2b87fa26096c585cc989417e2bc3447a4f1c
dominic-source <chinonsodomnic@gmail.com>
2024-07-09 17:48:22 +0100

    update app

--- app.py
+++ app.py
@@ -54,4 +54,4 @@ if __name__ == "__main__":
 
     # Run the Flask app using Uvicorn
     import uvicorn
-    uvicorn.run(app, host="127.0.0.2", port=5000)
+    uvicorn.run(app, host="0.0.0.0", port=5000)
